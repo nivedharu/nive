8af2bbacaf5eb2e169c2d0c0e0ecccfcdde5cbb2
nivedharu <Nivethadharani96@gmail.com>
2016-09-13 09:24:07 +0530

    Update and rename .gitignore to even.java

---
+++ even.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class even
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a>0)
+{
+System.out.println("positive");
+}
+else
+{
+System.out.println("negative");
+}
+}
+}
